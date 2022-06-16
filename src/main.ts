3fb3035bcb6a170d273928735b34d82ba5fe473f
olivan139 <oiivanov@edu.hse.ru>
2022-06-17 00:05:00 +0300

    v2

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import * as bodyParser from 'body-parser';
 async function start() {
   const PORT =  process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule)
-  app.enableCors();
   app.use(bodyParser.json({limit: '50mb'}));
   app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
   await app.listen(PORT, () => console.log("SERVER STARTED ON PORT = " + PORT))
